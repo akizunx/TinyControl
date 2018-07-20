e936f42038435bc31c4f246131c4fabbb82f7069
akizunx <17682291+akizunx@users.noreply.github.com>
2018-07-20 10:30:40 +0800

    chore: update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
+import sys
+
 from setuptools import setup, find_packages
 
-PACKAGES = ['tcontrol'] + ["tcontrol.%s" % i for i in find_packages('tcontrol')]
+if sys.version_info[: 2] < (3, 5):
+    raise RuntimeError('Python version >= 3.5 required')
+
+MAJOR = 0
+MINOR = 7
+MICRO = 11
+VERSION = f'{MAJOR}.{MINOR}.{MICRO}'
+
+PACKAGES = ['tcontrol'] + [f"tcontrol.{i}" for i in find_packages('tcontrol')]
 
 setup(
-    name='tinyControl',
-    version='0.6.0',
+    name='TinyControl',
+    version=VERSION,
+    author='TinyControl Developers',
     packages=PACKAGES,
     license='BSD 3-clause',
-    install_requires=['sympy >= 1.0', 'numpy >= 1.12.0', 'matplotlib >= 2.0.0']
+    install_requires=['sympy>=1.0', 'numpy>=1.12.0', 'matplotlib>=2.0.0'],
+    tests_requires=['nose2>=0.7.4'],
+    extra_requires={
+        'doc': ['sphinx>=1.6.6', 'sphinx-rtd-theme>=0.4.0']
+    }
 )
